68d9964ab8b859cf7ffee8cb9655a2f35944ca57
Sergio Fernández <sergio.fernandezj@gmail.com>
2017-11-21 14:26:28 +0100

    Now, you can play with your keyboard agaist the cpu

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -34,10 +36,38 @@ func main() {
 	g.addPlayer(p1)
 	g.addPlayer(p2)
 
-	fmt.Println(g.play())
+	fmt.Printf(`Game starts. Please select one:
+1 - %v
+2 - %v
+3 - %v
+0 - Exit program
+`, strings.Title(gameOptions[0]),
+		strings.Title(gameOptions[1]),
+		strings.Title(gameOptions[2]))
+
+	for {
+		var option int
+		fmt.Print("Enter option: ")
+		_, err := fmt.Scanf("%d", &option)
+		if err != nil {
+			fmt.Println("Error while reading input.")
+			os.Exit(1)
+		}
+		if option == 0 {
+			fmt.Println("See you next time.")
+			os.Exit(1)
+		}
+
+		if option > len(gameOptions) {
+			fmt.Println("Invalid option. Choose another one")
+			continue
+		}
+
+		fmt.Println(g.play(gameOptions[option-1]))
+	}
 }
 
-func (player) rockPaperScissor() string {
+func rockPaperScissor() string {
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
 
@@ -49,14 +79,13 @@ func (g *game) addPlayer(p player) {
 	g.players = append(g.players, p)
 }
 
-func (g game) play() string {
-	p1Chosen := g.players[0].rockPaperScissor()
-	p2Chosen := g.players[1].rockPaperScissor()
+func (g game) play(playerOption string) string {
+	cpuOption := rockPaperScissor()
 
 	var result string
-	if p1Chosen == p2Chosen {
+	if playerOption == cpuOption {
 		result = "Draw"
-	} else if rules[p1Chosen] == p2Chosen {
+	} else if rules[playerOption] == cpuOption {
 		result = fmt.Sprintf("%v won", g.players[0].name)
 	} else {
 		result = fmt.Sprintf("%v won", g.players[1].name)
